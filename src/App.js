0fca4fc3ad507f0510239d6eb3be260df29753fd
Joon <jaejoon0721@ajou.ac.kr>
2022-08-02 13:59:47 +0900

    리액트 해왔던거 업로드

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(props){
+  console.log('propss', props, props.title);
+  return (
+    <header>
+      <h1>
+        <a href='/' onClick={(event)=>{
+          event.preventDefault();
+          props.onChangeMode22();
+        }}>{props.title}</a>
+      </h1>
+    </header>
+  )
+}
+
+function Nav(props){
+  const lis = [
+
+]
+for(let i=0; i<props.topics.length; i++)
+{
+  let t = props.topics[i];
+  lis.push(<li key={t.id}>
+    <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
+    event.preventDefault();
+    props.onChangeMode22(event.target.id);
+  }}>{t.title} </a></li>)
+}
+  return(
+    <nav>
+      <ol>
+      {lis}
+    </ol>
+    </nav>
+  )
+}
+
+function Article(props){
+  return(
+    <article>
+      <h2>
+        {props.title}
+      </h2>
+      {props.body}
+    </article>
+  )
+}
+
+function App(){
+  const topics = [
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'Css is...'},
+    {id:3, title:'javascript', body:'js is...'}
+  ]
+  return(
+    <div>
+    <Header title="REACT" onChangeMode22={()=>{alert('Header');}}></Header>
+    <Nav topics={topics} onChangeMode22={(id)=>{alert(id);}}></Nav>
+    <Article title="Welcome2" body="Hello, React"></Article>
+      
+    </div>
+    )
+}
+export default App;
